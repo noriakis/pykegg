6fdab7195a6fff5b5dd7560f14768118dce81bd8
noriakis <nori@hgc.jp>
2023-05-15 14:26:07 +0900

    modified KGML_graph and utils

--- src/pykegg/KGML_graph.py
+++ src/pykegg/KGML_graph.py
@@ -1,152 +1,280 @@
-import re
-from Bio.KEGG.KGML.KGML_parser import read
+# import re
+import os
+import igraph
+import requests
+
 import pandas as pd
 import numpy as np
-import igraph
-from PIL import Image
-import cv2
-import matplotlib.pyplot as plt
-import os
-import requests
+
+from Bio.KEGG.KGML.KGML_parser import read
+
 
 class KGML_graph:
     def __init__(self, path=None, pid=None):
-        if path!=None and os.path.isfile(path):
-            self.pathway = read(open(path, 'r'))
+        if path is not None and os.path.isfile(path):
+            self.pathway = read(open(path, "r"))
         else:
-            if pid!=None:
-                pid_pref = "".join(re.findall("[a-zA-Z]+", pid))
-                path_cont = requests.get("https://rest.kegg.jp/get/"+pid+"/kgml")
+            if pid is not None:
+                # pid_pref = "".join(re.findall("[a-zA-Z]+", pid))
+                path_cont = requests.get("https://rest.kegg.jp/get/" + pid + "/kgml")
                 self.pathway = read(path_cont.content.decode())
-                
+
     def get_graph(self, layout="native"):
-        ed = self.get_edges()
-        nd = self.get_nodes()
-        
-        nd["orig_id"] = nd.id
-        ed["entry1_orig_id"] = ed.entry1
-        ed["entry2_orig_id"] = ed.entry2
-        
-        nd.id = np.arange(0, nd.shape[0], 1)
-        nd.index = nd.id
-        
-        convert = dict(zip(nd.orig_id, nd.id))
-        
-        ed.entry1 = ed.entry1.replace(convert)
-        ed.entry2 = ed.entry2.replace(convert)
-        
-        g = igraph.Graph.DataFrame(ed, vertices=nd)
-        
-        if layout!="native":
-            layout = g.layout(layout)
+        edge_df = self.get_edges()
+        node_df = self.get_nodes()
+
+        node_df["orig_id"] = node_df.id
+        edge_df["entry1_orig_id"] = edge_df.entry1
+        edge_df["entry2_orig_id"] = edge_df.entry2
+
+        node_df.id = np.arange(0, node_df.shape[0], 1)
+        node_df.index = node_df.id
+
+        convert = dict(zip(node_df.orig_id, node_df.id))
+
+        edge_df.entry1 = edge_df.entry1.replace(convert)
+        edge_df.entry2 = edge_df.entry2.replace(convert)
+
+        graph = igraph.Graph.DataFrame(edge_df, vertices=node_df)
+
+        if layout != "native":
+            layout = graph.layout(layout)
             layout = pd.DataFrame([l for l in layout])
             layout.columns = ["x", "y"]
-            g.vs["x"] = layout.x
-            g.vs["y"] = layout.y
-        return(g)
+            graph.vs["x"] = layout.x
+            graph.vs["y"] = layout.y
+        return graph
 
     def get_edges(self):
-        rel_list = [[r.entry1.id, r.entry2.id, r.type, r.subtypes, None] for r in self.pathway.relations]
-        if (len(self.pathway.reactions)!=0):
+        rel_list = [
+            [
+                relation.entry1.id,
+                relation.entry2.id,
+                relation.type,
+                relation.subtypes,
+                None,
+            ]
+            for relation in self.pathway.relations
+        ]
+        if len(self.pathway.reactions) != 0:
             reacs = []
-            for r in self.pathway.reactions:
-                for s in r.substrates:
-                    for p in r.products:
-                        reacs.append([s.id, p.id, None, None, r.name])
+            for reaction in self.pathway.reactions:
+                for substrate in reaction.substrates:
+                    for product in reaction.products:
+                        reacs.append(
+                            [substrate.id, product.id, None, None, reaction.name]
+                        )
             reacs = pd.DataFrame(reacs)
         else:
             reacs = None
-            
+
         edges = pd.DataFrame(rel_list)
         edges = pd.concat([edges, reacs])
         edges.index = np.arange(0, edges.shape[0], 1)
-        
-        if (edges.shape[0]==0):
-            return(None)
-        edges.columns = ["entry1","entry2","type","subtypes","reaction"]
-        
-        return(edges)
-    
-    def get_nodes(self, node_x_nudge=5, node_y_nudge=5):
 
-        
-        compounds = pd.DataFrame([c[0] for c in [[[j.id, j.name, element.x, element.y, j.name, element.coords, element.type,
-                        element.width, element.height, element.fgcolor, element.bgcolor,
-            [k.id for k in j.components]] for element in j.graphics] 
-           for j in [self.pathway.entries[i] for i in self.pathway.entries
-                     if self.pathway.entries[i].type=="compound"]]])
+        if edges.shape[0] == 0:
+            return None
+        edges.columns = ["entry1", "entry2", "type", "subtypes", "reaction"]
+
+        return edges
+
+    def get_nodes(self, node_x_nudge=5, node_y_nudge=5):
+        compounds = pd.DataFrame(
+            [
+                c[0]
+                for c in [
+                    [
+                        [
+                            j.id,
+                            j.name,
+                            element.x,
+                            element.y,
+                            j.name,
+                            element.coords,
+                            element.type,
+                            element.width,
+                            element.height,
+                            element.fgcolor,
+                            element.bgcolor,
+                            [k.id for k in j.components],
+                        ]
+                        for element in j.graphics
+                    ]
+                    for j in [
+                        self.pathway.entries[i]
+                        for i in self.pathway.entries
+                        if self.pathway.entries[i].type == "compound"
+                    ]
+                ]
+            ]
+        )
         compounds["original_type"] = "compound"
 
-        if len(self.pathway.genes)!=0:
-            genes = [[[gene.id, gene.name, element.x, element.y, element.name, element.coords, element.type,
-                element.width, element.height, element.fgcolor, element.bgcolor, None] for element in gene.graphics] 
-                     for gene in self.pathway.genes]
+        if len(self.pathway.genes) != 0:
+            genes = [
+                [
+                    [
+                        gene.id,
+                        gene.name,
+                        element.x,
+                        element.y,
+                        element.name,
+                        element.coords,
+                        element.type,
+                        element.width,
+                        element.height,
+                        element.fgcolor,
+                        element.bgcolor,
+                        None,
+                    ]
+                    for element in gene.graphics
+                ]
+                for gene in self.pathway.genes
+            ]
             genes = pd.DataFrame([g[0] for g in genes])
             genes["original_type"] = "gene"
         else:
             genes = None
 
-        if len(self.pathway.orthologs)!=0:
-            orthos = [[[ortho.id, ortho.name, element.x, element.y, element.name, element.coords, element.type,
-                element.width, element.height, element.fgcolor, element.bgcolor, None] for element in ortho.graphics] 
-                     for ortho in self.pathway.orthologs]
+        if len(self.pathway.orthologs) != 0:
+            orthos = [
+                [
+                    [
+                        ortho.id,
+                        ortho.name,
+                        element.x,
+                        element.y,
+                        element.name,
+                        element.coords,
+                        element.type,
+                        element.width,
+                        element.height,
+                        element.fgcolor,
+                        element.bgcolor,
+                        None,
+                    ]
+                    for element in ortho.graphics
+                ]
+                for ortho in self.pathway.orthologs
+            ]
             orthos = pd.DataFrame([g[0] for g in orthos])
             orthos["original_type"] = "ortholog"
         else:
             orthos = None
-            
-        maps = [[[m.id, m.name, element.x, element.y, element.name, element.coords, element.type,
-            element.width, element.height, element.fgcolor, element.bgcolor, None] for element in m.graphics] 
-                 for m in self.pathway.maps]
+
+        maps = [
+            [
+                [
+                    m.id,
+                    m.name,
+                    element.x,
+                    element.y,
+                    element.name,
+                    element.coords,
+                    element.type,
+                    element.width,
+                    element.height,
+                    element.fgcolor,
+                    element.bgcolor,
+                    None,
+                ]
+                for element in m.graphics
+            ]
+            for m in self.pathway.maps
+        ]
         maps = pd.DataFrame([m[0] for m in maps])
         maps["original_type"] = "map"
-        
-        groups = [[[j.id, j.name, element.x, element.y, j.name, element.coords, element.type,
-                                element.width, element.height, element.fgcolor, element.bgcolor,
-                    [k.id for k in j.components]] for element in j.graphics] 
-                   for j in [self.pathway.entries[i] for i in self.pathway.entries
-                             if self.pathway.entries[i].type=="group"]]
+
+        groups = [
+            [
+                [
+                    j.id,
+                    j.name,
+                    element.x,
+                    element.y,
+                    j.name,
+                    element.coords,
+                    element.type,
+                    element.width,
+                    element.height,
+                    element.fgcolor,
+                    element.bgcolor,
+                    [k.id for k in j.components],
+                ]
+                for element in j.graphics
+            ]
+            for j in [
+                self.pathway.entries[i]
+                for i in self.pathway.entries
+                if self.pathway.entries[i].type == "group"
+            ]
+        ]
         groups = pd.DataFrame([g[0] for g in groups])
         groups["original_type"] = "group"
-        
+
         nodes = pd.concat([genes, maps, groups, compounds, orthos])
-        
-        nodes.columns = ["id","name","x","y","graphics_name","coords","type",
-                        "width","height","fgcolor","bgcolor","group","original_type"]
-        
+
+        nodes.columns = [
+            "id",
+            "name",
+            "x",
+            "y",
+            "graphics_name",
+            "coords",
+            "type",
+            "width",
+            "height",
+            "fgcolor",
+            "bgcolor",
+            "group",
+            "original_type",
+        ]
+
         nodes["xmin"] = nodes["x"] - nodes["width"] + node_x_nudge
         nodes["xmax"] = nodes["x"] + nodes["width"] - node_x_nudge
-        
-        nodes["y"] = nodes["y"]*-1
+
+        nodes["y"] = nodes["y"] * -1
         nodes["ymin"] = nodes["y"] - nodes["height"] + node_y_nudge
         nodes["ymax"] = nodes["y"] + nodes["height"] - node_y_nudge
-        
-        return(nodes)
-    
+
+        return nodes
+
     def get_coords(self):
-        all_ortho=[]
+        all_ortho = []
         for ortho in self.pathway.orthologs:
             graphics = ortho.graphics
             for element in graphics:
                 seg_list = []
-                if element.coords==None:
+                if element.coords is None:
                     break
-                for i, co in enumerate(element.coords):
-                    if i < len(element.coords)-1:
-                        seg_list.append((
-                            element.coords[i][0],
-                            element.coords[i][1],
-                            element.coords[i+1][0],
-                            element.coords[i+1][1],
-                            element.name,
-                            element.type,
-                            element.fgcolor,
-                            element.bgcolor))
+                for i in range(len(element.coords)):
+                    if i < len(element.coords) - 1:
+                        seg_list.append(
+                            (
+                                element.coords[i][0],
+                                element.coords[i][1],
+                                element.coords[i + 1][0],
+                                element.coords[i + 1][1],
+                                element.name,
+                                element.type,
+                                element.fgcolor,
+                                element.bgcolor,
+                            )
+                        )
                 all_ortho.append(pd.DataFrame(seg_list))
-        if (len(all_ortho)==0):
-            return(None)
+        if len(all_ortho) == 0:
+            return None
         coords = pd.concat(all_ortho)
-        coords.columns = ["x","y","xend","yend","name","type","fgcolor","bgcolor"]
-        coords["y"] = coords["y"]*-1
-        coords["yend"] = coords["yend"]*-1
-        return(coords)
+        coords.columns = [
+            "x",
+            "y",
+            "xend",
+            "yend",
+            "name",
+            "type",
+            "fgcolor",
+            "bgcolor",
+        ]
+        coords["y"] = coords["y"] * -1
+        coords["yend"] = coords["yend"] * -1
+        return coords
--- src/pykegg/utils.py
+++ src/pykegg/utils.py
@@ -1,129 +1,219 @@
-import requests
-import numpy as np
 import re
 import os
-import matplotlib as mpl
-from PIL import Image
+import requests
 import cv2
+
+import numpy as np
+import matplotlib as mpl
 import pandas as pd
-from plotnine import ggplot, geom_point, aes, geom_segment, theme_void, options, geom_rect, geom_text, geom_raster, geom_path
+
+from PIL import Image
+from plotnine import (
+    ggplot,
+    geom_point,
+    aes,
+    geom_segment,
+    theme_void,
+    geom_rect,
+    geom_text
+)
 
 
-def overlay_opencv_image(nds, 
-                         path=None, 
-                         pid=None,
-                         fill_color="color", 
-                         transparent_colors=["#FFFFFF","#BFFFBF"]):
-    nds["x0"] = nds["x"] - nds["width"] /2
-    nds["y0"] = nds["y"] + nds["height"] /2
-        
-    if path!=None and os.path.isfile(path):
+def overlay_opencv_image(
+    node_df,
+    path=None,
+    pid=None,
+    fill_color="color",
+    transparent_colors=None,
+):
+    """Obtain the raw image of pathway and color the nodes.
+
+    Parameters:
+    -----------
+    node_df: DataFrame
+        node data obtained by `get_nodes()`.
+    path: str
+        path to the image if already downloaded.
+    pid: str
+        KEGG pathway identifier.
+    fill_color: str
+        the column in `node_df` specifying color in HEX.
+        If list is given, split the width according to
+        the color number.
+    transparent_color: list of str
+        specify which color to be transparent.
+
+    """
+    if transparent_colors is None:
+        transparent_colors = ["#FFFFFF", "#BFFFBF"]
+
+    node_df["x0"] = node_df["x"] - node_df["width"] / 2
+    node_df["y0"] = node_df["y"] + node_df["height"] / 2
+
+    if path is not None and os.path.isfile(path):
         im = cv2.imread(path)
     else:
-        if pid!=None:
+        if pid is not None:
             pid_pref = "".join(re.findall("[a-zA-Z]+", pid))
-            im_res = requests.get("https://www.kegg.jp/kegg/pathway/"+pid_pref+"/"+pid+".png")
+            im_res = requests.get(
+                "https://www.kegg.jp/kegg/pathway/" + pid_pref + "/" + pid + ".png"
+            )
             nparr = np.frombuffer(im_res.content, np.uint8)
             im = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    canvas = np.zeros([im.shape[0],im.shape[1],3],dtype=np.uint8)
+    canvas = np.zeros([im.shape[0], im.shape[1], 3], dtype=np.uint8)
     canvas.fill(255)
-    
+
     dst = cv2.cvtColor(im, cv2.COLOR_BGR2BGRA)
     for col in transparent_colors:
         h = col[1:7]
-        cand_color = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
-        mask = np.all(im[:,:,:] == list(cand_color), axis=-1)
-        dst[mask,3] = 0
-        
-    for i in nds.index:
-        tmp = nds.iloc[i,:]
-        pos = (int(tmp["x0"]),
-               int(-1*tmp["y0"]),
-               int(tmp["width"]),
-               int(tmp["height"]))
+        cand_color = tuple(int(h[i : i + 2], 16) for i in (0, 2, 4))
+        mask = np.all(im[:, :, :] == list(cand_color), axis=-1)
+        dst[mask, 3] = 0
 
-        tmp_col = tmp["color"]
+    for i in node_df.index:
+        tmp = node_df.iloc[i, :]
+        pos = (
+            int(tmp["x0"]),
+            int(-1 * tmp["y0"]),
+            int(tmp["width"]),
+            int(tmp["height"]),
+        )
+
+        tmp_col = tmp[fill_color]
         if isinstance(tmp_col, list):
             num_col = len(tmp_col)
             nudge = tmp["width"] / num_col
             for e, one_tmp_col in enumerate(tmp_col):
-                new_width = tmp["width"] - (nudge*e)
-                canvas = cv2.rectangle(img=canvas,
-                           rec=(int(tmp["x0"]),
-                            int(-1*tmp["y0"]),
-                            int(new_width),int(tmp["height"])),
-                           color=hex2rgb(one_tmp_col),
-                           thickness=-1)          
+                new_width = tmp["width"] - (nudge * e)
+                canvas = cv2.rectangle(
+                    img=canvas,
+                    rec=(
+                        int(tmp["x0"]),
+                        int(-1 * tmp["y0"]),
+                        int(new_width),
+                        int(tmp["height"]),
+                    ),
+                    color=hex2rgb(one_tmp_col),
+                    thickness=-1,
+                )
 
         else:
-            canvas = cv2.rectangle(img=canvas,
-                       rec=pos, color=hex2rgb(tmp_col),
-                       thickness=-1)
+            canvas = cv2.rectangle(
+                img=canvas, rec=pos, color=hex2rgb(tmp_col), thickness=-1
+            )
 
     image = overlay(canvas, dst)
-    return(image)
+    return image
+
 
 def overlay(rects, kegg_map):
     rects = cv2.cvtColor(rects, cv2.COLOR_BGR2RGB)
     rects = Image.fromarray(rects).convert("RGBA")
     kegg_map = cv2.cvtColor(kegg_map, cv2.COLOR_BGRA2RGBA)
-    kegg_map = Image.fromarray(kegg_map).convert('RGBA')
+    kegg_map = Image.fromarray(kegg_map).convert("RGBA")
     result_image = Image.alpha_composite(rects, kegg_map)
     return cv2.cvtColor(np.asarray(result_image), cv2.COLOR_RGBA2BGRA)
 
-def plot_kegg_pathway_plotnine(g, node_x_nudge=20, node_y_nudge=10,
-                              split_graphics_name=True, subtype_num=0,
-                               label_size=2, show_label="gene",
-                              edge_color="subtype", text_label="graphics_name"):
+
+def plot_kegg_pathway_plotnine(
+    g,
+    node_x_nudge=20,
+    node_y_nudge=10,
+    split_graphics_name=True,
+    subtype_num=0,
+    label_size=2,
+    show_label="gene",
+    edge_color="subtype",
+    text_label="graphics_name",
+):
     nddf = g.get_nodes(node_x_nudge=node_x_nudge, node_y_nudge=node_y_nudge)
     eddf = g.get_edges()
     if split_graphics_name:
         nddf["graphics_name"] = nddf.graphics_name.apply(lambda x: x.split(",")[0])
 
-    seg_df = pd.concat([
-        nddf.reset_index().set_index("id").loc[eddf.entry1].reset_index().loc[:,["x","y"]],
-        nddf.reset_index().set_index("id").loc[eddf.entry2].reset_index().loc[:,["x","y"]]
-    ], axis=1)
-    seg_df.columns = ["x","y", "xend", "yend"]
+    seg_df = pd.concat(
+        [
+            nddf.reset_index()
+            .set_index("id")
+            .loc[eddf.entry1]
+            .reset_index()
+            .loc[:, ["x", "y"]],
+            nddf.reset_index()
+            .set_index("id")
+            .loc[eddf.entry2]
+            .reset_index()
+            .loc[:, ["x", "y"]],
+        ],
+        axis=1,
+    )
+    seg_df.columns = ["x", "y", "xend", "yend"]
     seg_df = pd.concat([seg_df, eddf], axis=1)
 
-    seg_df["subtype"] = seg_df.subtypes.apply(lambda x: x[0][subtype_num] if x!=None else x)
-    
-    plot = (ggplot() +
-     geom_segment(aes(x="x", y="y",xend="xend",yend="yend",color=edge_color), data=seg_df) + 
-     geom_rect(aes(xmin="xmin", ymin="ymin", xmax="xmax", ymax="ymax"),
-               data=nddf[nddf.original_type=="gene"], fill="white", color="grey") +
-     geom_rect(aes(xmin="xmin", ymin="ymin", xmax="xmax", ymax="ymax"),
-               data=nddf[nddf.original_type=="compound"], fill="white", color="grey") +
-     geom_text(aes(x="x", y="y", label=text_label, filter="original_type!='group'"),
-               data=nddf[nddf.original_type==show_label], size=label_size)+theme_void())
-    return(plot)
+    seg_df["subtype"] = seg_df.subtypes.apply(
+        lambda x: x[0][subtype_num] if x is not None else x
+    )
 
-def plot_kegg_global_map_plotnine(g, hide_map=True):
-    nds = g.get_nodes()
-    coords = g.get_coords()
+    plot = (
+        ggplot()
+        + geom_segment(
+            aes(x="x", y="y", xend="xend", yend="yend", color=edge_color), data=seg_df
+        )
+        + geom_rect(
+            aes(xmin="xmin", ymin="ymin", xmax="xmax", ymax="ymax"),
+            data=nddf[nddf.original_type == "gene"],
+            fill="white",
+            color="grey",
+        )
+        + geom_rect(
+            aes(xmin="xmin", ymin="ymin", xmax="xmax", ymax="ymax"),
+            data=nddf[nddf.original_type == "compound"],
+            fill="white",
+            color="grey",
+        )
+        + geom_text(
+            aes(x="x", y="y", label=text_label, filter="original_type!='group'"),
+            data=nddf[nddf.original_type == show_label],
+            size=label_size,
+        )
+        + theme_void()
+    )
+    return plot
+
+
+def plot_kegg_global_map_plotnine(graph, hide_map=True):
+    node_df = graph.get_nodes()
+    coords = graph.get_coords()
     if hide_map:
-        nds = nds[nds.original_type!="map"]
-    plt = (ggplot() +
-     geom_segment(aes(x="x",y="y", xend="xend", yend="yend"),
-                  color=coords["fgcolor"].tolist(), data=coords) + 
-     geom_point(aes(x="x",y="y"), color=nds["fgcolor"].tolist(), data=nds)+
-     theme_void())
-    return(plt)
+        node_df = node_df[node_df.original_type != "map"]
+    plt = (
+        ggplot()
+        + geom_segment(
+            aes(x="x", y="y", xend="xend", yend="yend"),
+            color=coords["fgcolor"].tolist(),
+            data=coords,
+        )
+        + geom_point(aes(x="x", y="y"), color=node_df["fgcolor"].tolist(), data=node_df)
+        + theme_void()
+    )
+    return plt
 
-def color_grad(minimum=-2,maximum=2,seq=0.1,
-              min_col="#ffffff",max_col="#ff0000",
-              round_num=2):
-    minmax = np.arange(minimum,maximum,seq)
-    N = len(minmax)
-    conv={}
-    for x in range(N):
-        l=np.round(minmax[x],round_num)
-        c1=np.array(mpl.colors.to_rgb(min_col))
-        c2=np.array(mpl.colors.to_rgb(max_col))
-        conv[l] = mpl.colors.to_hex((1-x/N)*c1 + x/N*c2)
-    return(conv)
 
-def hex2rgb(h):
-    return(tuple(int(h.lstrip("#")[i:i+2], 16) for i in (0, 2, 4)))
+def color_grad(
+    minimum=-2, maximum=2, seq=0.1,
+    min_col="#ffffff", max_col="#ff0000",
+    round_num=2
+):
+    minmax = np.arange(minimum, maximum, seq)
+    num_seq = len(minmax)
+    conv = {}
+    for num in range(num_seq):
+        rounded = np.round(minmax[num], round_num)
+        color1 = np.array(mpl.colors.to_rgb(min_col))
+        color2 = np.array(mpl.colors.to_rgb(max_col))
+        conv[rounded] = mpl.colors.to_hex((1 - num / num_seq) * color1 + num / num_seq * color2)
+    return conv
+
+
+def hex2rgb(hex_str):
+    return tuple(int(hex_str.lstrip("#")[i : i + 2], 16) for i in (0, 2, 4))
