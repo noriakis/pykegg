d8741e7d1aa561cd3f2759f22165c28e917bd4f3
noriakis <nori@hgc.jp>
2023-06-02 12:12:11 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
+    # 'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
 
