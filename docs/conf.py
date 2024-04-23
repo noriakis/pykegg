c1e2eea2f334ce13f64f4d7a2dcde5b32d4088d5
noriakis <nori@hgc.jp>
2024-04-23 14:31:12 +0900

    [rtd] module first

--- docs/conf.py
+++ docs/conf.py
@@ -54,8 +54,8 @@ def run_apidoc(_):
     import sys
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
     cur_dir = os.path.abspath(os.path.dirname(__file__))
-    module = os.path.join(cur_dir,"..","src", "pykegg")
-    main(['--no-toc', '--force', '-e', '-o', cur_dir, module])
+    module = os.path.join(cur_dir,"..","src")
+    main(['--module-first', '--no-toc', '--force', '-e', '-o', cur_dir, module])
 
 def setup(app):
     app.connect('builder-inited', run_apidoc)
