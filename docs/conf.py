44d8cc4cfb51276dfb8641c1d6a848f3be43a1f2
noriakis <nori@hgc.jp>
2024-04-23 15:37:23 +0900

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ def run_apidoc(_):
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
     cur_dir = os.path.abspath(os.path.dirname(__file__))
     module = os.path.join(cur_dir,"..","src")
-    main(['--force', '--no-toc',
+    main(['--force',
         '-e', '-o', cur_dir, module])
 
 def setup(app):
